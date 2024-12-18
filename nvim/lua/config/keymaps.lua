80a52ae11894db84de111a691092d3f5f00a1214
Rian Rainey <c-rian.rainey@vimeo.com>
2024-12-18 12:44:21 -0500

    Add tmux catpuccin themes that I didn't end up using
    typecraft likes catpuccin. It didn't jive w/ my terminal colors so I
    skipped it but wanted to keep the code in case I fix my terminal colors
    and go back.

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,9 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set(
-  "n",
-  "<leader>sx",
-  require("telescope.builtin").resume,
-  { noremap = true, silent = true, desc = "Resume" }
-)
+-- vim.keymap.set(
+--   "n",
+--   "<leader>sx",
+--   require("telescope.builtin").resume,
+--   { noremap = true, silent = true, desc = "Resume" }
+-- )
