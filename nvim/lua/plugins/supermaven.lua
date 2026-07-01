5c3b2a6ca4afd5e155a2dae6574706267bc22858
Rian Rainey <rianrainey@gmail.com>
2026-07-01 12:45:28 -0400

    Fix Neovim LazyVim setup

--- nvim/lua/plugins/supermaven.lua
+++ nvim/lua/plugins/supermaven.lua
@@ -1,7 +1,9 @@
 return {
   "supermaven-inc/supermaven-nvim",
   config = function()
-    require("supermaven-nvim").setup({})
+    require("supermaven-nvim").setup({
+      log_level = "off",
+    })
   end,
 }
 -- require("lazy").setup({
